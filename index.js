5f2d7f6ec8f03cbd41f3afcaf20aac0965e6c9f0
Kai <katzkuma@gmail.com>
2023-08-30 13:00:07 +1000

    chore: start ignore directory dist/ for git

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const port = 8000;
-const app = express();
-app.get('/', (req, res) => {
-    res.send('Hello Express + Typescript');
-});
-app.listen(port, () => {
-    console.log(`Server runing on port ${port}`);
-});
